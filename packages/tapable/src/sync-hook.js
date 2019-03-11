05f98eb9b0829654b7c316fc812122761c6395da
Huang Jules <jules3in@gmail.com>
2019-03-11 11:25:47 +0800

    update: tapable example, sync-hook

--- packages/tapable/src/sync-hook.js
+++ packages/tapable/src/sync-hook.js
@@ -2,10 +2,14 @@ const { SyncHook } = require("tapable");
 
 const cat = new SyncHook(["fish"]);
 
-const eat = (part, ...args) => console.log.call(null, `(cat eat ${part} of)`, ...args);
+const eat = (part, ...args) =>
+  console.log.call(null, `(cat eat ${part} of)`, ...args);
 
 cat.tap("eat head", (fish, fish2) => {
   eat("head", fish, fish2);
+  if (fish === "tuna") {
+    throw new Error("I don't eat tuna head!");
+  }
   // `fish2` won't be taken because the cat take one fish only!
 });
 
@@ -18,3 +22,10 @@ cat.tap("eat tail", fish => {
 });
 
 cat.call("carp", "tuna");
+
+console.log();
+try {
+  cat.call("tuna");
+} catch (error) {
+  console.log(`Cat cries: "${error.message}", and lies down dead.`);
+}
