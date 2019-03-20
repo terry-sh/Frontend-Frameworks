71f0cdec9c426a2bfebe192038ef1efd521542e3
Huang Jules <jules3in@gmail.com>
2019-03-20 19:03:04 +0800

    add: comment, to typescript. lib/transformer

--- packages/typescript/src/lib/transformer.ts
+++ packages/typescript/src/lib/transformer.ts
@@ -1,11 +1,53 @@
 import * as ts from "typescript";
 import * as path from "path";
 
-export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
-  return (context: ts.TransformationContext) => (file: ts.SourceFile) =>
-    visitNodeAndChildren(file, program, context);
+const DispatchName = "Dispatch";
+
+/** Type declration file name */
+const TypeDeclarationFile = "index.ts";
+
+/** Absolute path of type declration file */
+const indexTs = path.join(__dirname, TypeDeclarationFile);
+
+/**
+ * Whether the current node is a "dispatch" call, i.e.
+ * 
+ * ```ts
+ * dispatch({ type: 'user/fetchUser', payload; { userId: 1 } })
+ * ```
+ * 
+ * @param node
+ * @param typeChecker
+ */
+function isDispatchCallExpression(
+  node: ts.Node,
+  typeChecker: ts.TypeChecker
+): node is ts.CallExpression {
+  if (node.kind === ts.SyntaxKind.CallExpression) {
+    const signature = typeChecker.getResolvedSignature(node as ts.CallExpression);
+    if (signature !== undefined) {
+      const { declaration } = signature;
+      if (!!declaration) {
+        if (ts.isCallSignatureDeclaration(declaration)) {
+          if (ts.isInterfaceDeclaration(declaration.parent)) {
+            const isDispatch = declaration.parent.name.getText() === "Dispatch";
+            if (isDispatch) {
+              return path.join(declaration.getSourceFile().fileName) === indexTs;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
 }
 
+/**
+ * Recursive node visitor of Typescript compiler
+ * @param node 
+ * @param program 
+ * @param context 
+ */
 function visitNodeAndChildren(
   node: ts.SourceFile,
   program: ts.Program,
@@ -28,6 +70,11 @@ function visitNodeAndChildren(
   );
 }
 
+/**
+ * Node visitor of Typescript compiler
+ * @param node 
+ * @param program 
+ */
 function visitNode(node: ts.Node, program: ts.Program): ts.Node {
   const typeChecker = program.getTypeChecker();
   if (!isDispatchCallExpression(node, typeChecker)) {
@@ -37,41 +84,28 @@ function visitNode(node: ts.Node, program: ts.Program): ts.Node {
   if (node.arguments.length > 0) {
     const payload = node.arguments[0];
     if (ts.isObjectLiteralExpression(payload)) {
-      payload.properties = ts.createNodeArray(payload.properties.map(val => {
-        if (ts.isPropertyAssignment(val) && val.name.getText() === "type") {
-          const { initializer } = val;
-          if (ts.isPropertyAccessExpression(initializer)) {
-            val.initializer = ts.createStringLiteral(initializer.getText());
+      payload.properties = ts.createNodeArray(
+        payload.properties.map(val => {
+          if (ts.isPropertyAssignment(val) && val.name.getText() === DispatchName) {
+            const { initializer } = val;
+            if (ts.isPropertyAccessExpression(initializer)) {
+              val.initializer = ts.createStringLiteral(initializer.getText());
+            }
           }
-        }
-        return val;
-      }));
+          return val;
+        })
+      );
     }
   }
   return node;
 }
 
-const indexTs = path.join(__dirname, "index.ts");
-
-function isDispatchCallExpression(
-  node: ts.Node,
-  typeChecker: ts.TypeChecker
-): node is ts.CallExpression {
-  if (node.kind === ts.SyntaxKind.CallExpression) {
-    const signature = typeChecker.getResolvedSignature(node as ts.CallExpression);
-    if (signature !== undefined) {
-      const { declaration } = signature;
-      if (!!declaration) {
-        if (ts.isCallSignatureDeclaration(declaration)) {
-          if (ts.isInterfaceDeclaration(declaration.parent)) {
-            const isDispatch = declaration.parent.name.getText() === 'Dispatch';
-            if (isDispatch) {
-              return path.join(declaration.getSourceFile().fileName) === indexTs;
-            }
-          }
-        }
-      }
-    }
-  }
-  return false;
+/**
+ * Transformer generator
+ * @param program
+ */
+export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
+  /** Transformer plugin */
+  return (context: ts.TransformationContext) => (file: ts.SourceFile) =>
+    visitNodeAndChildren(file, program, context);
 }
