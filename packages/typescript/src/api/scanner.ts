e648aabf55b2e0fa15b32ae3e4a3a350477af111
Huang Jules <jules3in@gmail.com>
2019-04-02 16:55:44 +0800

    [typescript] update: scaner samples

--- packages/typescript/src/api/scanner.ts
+++ packages/typescript/src/api/scanner.ts
@@ -2,12 +2,15 @@
 
 import * as ts from "typescript";
 
-const sourceCode = `
-const i: number = 1.2;
+const sourceCode = `const i: number = 1.2;
 const flag = 0b0011;
 const name = "jules";
 
 const cat: Cat = null;
+
+if (i > 0) {
+  console.log('"i" is positive.');
+}
 `;
 
 const tokenAnalyzer = ts.createScanner(ts.ScriptTarget.Latest, true);
@@ -18,7 +21,7 @@ tokenAnalyzer.setOnError((message: ts.DiagnosticMessage, length: number) => {
 
 // TODO: test different target script version
 tokenAnalyzer.setScriptTarget(ts.ScriptTarget.ES5);
-tokenAnalyzer.setLanguageVariant(ts.LanguageVariant.Standard);
+tokenAnalyzer.setLanguageVariant(ts.LanguageVariant.Standard); // standard or JSX
 
 const tokenList = [];
 while (true) {
@@ -27,22 +30,24 @@ while (true) {
     break;
   }
 
+  const startPos = tokenAnalyzer.getStartPos();
   const info = {
     token,
     kind: ts.SyntaxKind[token],
+    startPos,
     text: null
   };
 
   if (
     token === ts.SyntaxKind.Identifier ||
-    token === ts.SyntaxKind.FirstLiteralToken ||
-    token === ts.SyntaxKind.LastLiteralToken ||
+    token === ts.SyntaxKind.NumericLiteral || // FirstLiteralToken
     token === ts.SyntaxKind.BigIntLiteral ||
     token === ts.SyntaxKind.StringLiteral
-    ) {
-      const text = tokenAnalyzer.getTokenText();
-      info.text = text;
-    }
-    tokenList.push(info);
+  ) {
+    const text = tokenAnalyzer.getTokenText();
+    info.text = text;
+  }
+
+  tokenList.push(info);
 }
 console.table(tokenList);
